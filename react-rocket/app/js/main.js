8ce2ee78142b5d2381c285b88fd520eabccf76ee
堀口 誠人 <horiguchi_seito@liginc.co.jp>
2016-09-15 05:40:18 +0900

    fix 閉じタグ

--- react-rocket/app/js/main.js
+++ react-rocket/app/js/main.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import App from './components/App'
-import reducer from './reducers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import App from './components/App';
+import reducer from './reducers';
 
 const store = createStore(reducer);
 
