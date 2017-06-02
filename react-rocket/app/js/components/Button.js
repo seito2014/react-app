87f50ce2622a40878dbcb31942ff7ef3bcf43563
Seito Horiguchi <horiguchi_seito@liginc.co.jp>
2017-06-02 21:48:54 +0900

    update. button

--- react-rocket/app/js/components/Button.js
+++ react-rocket/app/js/components/Button.js
@@ -1,34 +1,4 @@
 import React, {Component, PropTypes} from 'react';
-import styled from 'styled-components';
-
-import constants from '../constants/Style.js';
-
-const ButtonStyle = styled.button`
-    display: block;
-    width: 100%;
-    color: #fff;
-    background-color: #000;
-    border-radius: 5px;
-    padding: 15px;
-    font-size: 20px;
-    font-weight: bold;
-    line-height: 1;
-    
-    &:hover {
-        opacity: .7;
-    }
-    
-    ${props => props.fb && `
-		background-color: ${constants.COLOR_FB};
-		font-style: italic;
-	`}
-	${props => props.tw && `
-		background-color: ${constants.COLOR_TW};
-	`}
-	${props => props.insta && `
-		background-color: ${constants.COLOR_INSTA};
-	`}
-`;
 
 class Button extends Component {
 
@@ -37,9 +7,9 @@ class Button extends Component {
         const {text, onClick} = this.props;
 
         return (
-            <ButtonStyle onClick={onClick}>
+            <Button className="button" onClick={onClick}>
                 {text}
-            </ButtonStyle>
+            </Button>
         )
     }
 }
